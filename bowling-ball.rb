b784e342bbd135e7906179e5394e6061ae30b59b
Leilanigreer <leilani@lpcgolf.com>
2024-08-25 14:55:51 -0700

    starting of bowling program

---
+++ bowling-ball.rb
@@ -0,0 +1,32 @@
+# Write code to keep track of the score of a game of bowling. It should have two methods: roll which takes in the number of pins knocked down, and score which is called at the end and returns the total game score.
+
+# take a number in and store it
+
+# score = 0
+# puts "how many pins did you hit? Your number must be bewteen 0-10"  
+# new_score = score + gets.to_i
+# p new_score
+
+
+
+# ask a user how many pins they hit the second time and add it to the first number they gave you
+
+# puts "how many pins did you hit on your second role? Still needs to be between 0-10"
+# new_score += gets.to_i
+# pp "Your new score is #{new_score}"
+
+
+
+# create the ability to randomly select a number between 0-10
+
+# random_number = rand(0...10)
+# pp random_number
+
+
+
+# use the random number created above and store it as "pins knocked down". 
+
+pins_knocked_down = rand(o...10)
+pp pins_knocked_down
+
+
